105babc9a33a0b6f1247a7d0760c670467870882
rahulsharma180 <rahulsharma18072022@gmail.com>
2024-09-14 17:46:39 +0530

    Healhty

--- Healthy/src/Component/Foodlist.jsx
+++ Healthy/src/Component/Foodlist.jsx
@@ -3,11 +3,11 @@
   let foodItems = ["dal","rice","roti","Milk","Salad","Quinoa Pulao", "Chia Pudding", "Idli", "Vegetable Upma", "Palak Paneer",]
   
   let food = foodItems.map((item)=>(
-      <li class="list-group-item"> {item}</li>
+      <li key={item}className="list-group-item"> {item}</li>
     ))
 
   return (
-   <> <div className="Mu"><ul key={item} class="list-group">
+   <> <div className ="Mu"><ul className="list-group">
    {food}
   </ul></div></>
   )
